762477a7976d10bba0cae674f5742c1eaff32bc9
unknown <sofikiereme43@gmail.com>
2026-09-08 11:01:05 -0400

    Initial commit

---
+++ app/src/main/java/com/example/a4x4_puzle/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.a4x4_puzle
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
